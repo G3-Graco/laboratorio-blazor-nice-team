697141630c1e3c219a4e59947b6f589c191cdcb7
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-04-19 13:38:22 -0400

    Creación de clases creadoras de movimientos

---
+++ APP/Components/Pages/Depositos/Deposito.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+namespace APP.Components.Pages.Depositos
+{
+    public partial class Deposito : ComponentBase
+    {
+        
+    }
+}
---
+++ APP/Components/Pages/Retiros/Retiro.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+namespace APP.Components.Pages.Retiros
+{
+    public partial class Retiro : ComponentBase
+    {
+        
+    }
+}
