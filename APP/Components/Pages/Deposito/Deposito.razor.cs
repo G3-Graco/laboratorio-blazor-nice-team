04471e3a0759bdaa5046895de98a8c27f5e452c6
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-04-19 13:56:55 -0400

    Cambio de ubicación de carpetas

--- APP/Components/Pages/Deposito/Deposito.razor.cs
+++ APP/Components/Pages/Deposito/Deposito.razor.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components;
 
-namespace APP.Components.Pages.Depositos
+namespace APP.Components.Pages.Deposito
 {
     public partial class Deposito : ComponentBase
     {
