82b11f9221feba841458701f84b2ef3222e9c693
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-04-19 14:17:24 -0400

    Con listar y solicitar prestamos

---
+++ APP/Components/Pages/ListaPrestamos/ListaPrestamos.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+namespace APP.Components.Pages.ListaPrestamos
+{
+    public partial class ListaPrestamos : ComponentBase
+    {
+        
+    }
+}
---
+++ APP/Components/Pages/SolicitarPrestamo/SolicitarPrestamo.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+namespace APP.Components.Pages.SolicitarPrestamo
+{
+    public partial class SolicitarPrestamo : ComponentBase
+    {
+        
+    }
+}
