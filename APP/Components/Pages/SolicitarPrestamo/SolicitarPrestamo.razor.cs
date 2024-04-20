49984854a72c4fcfb7fa02f305e3a77826c4d809
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-04-20 04:35:38 -0400

    Modificación a los préstamos

--- APP/Components/Pages/SolicitarPrestamo/SolicitarPrestamo.razor.cs
+++ APP/Components/Pages/SolicitarPrestamo/SolicitarPrestamo.razor.cs
@@ -2,12 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using APP.Data.Modelos;
+using APP.Data.Servicios;
+using BlazorBootstrap;
 using Microsoft.AspNetCore.Components;
 
 namespace APP.Components.Pages.SolicitarPrestamo
 {
     public partial class SolicitarPrestamo : ComponentBase
     {
-        
+        [Inject]
+		public NavigationManager Navigation { get; set; }
+
+		[Inject]
+		public PrestamoServicio prestamoServicio { get; set; }
+
+		public Prestamo prestamo = new Prestamo();
+
+		private Modal modal = default!;
+
+		protected override async Task OnInitializedAsync()
+        {
+            prestamo.Fecha = DateTime.Now;
+        }
+        public async void Solicitar()
+        {
+			
+		}
+
+		public async Task CerrarModal()
+		{
+			await modal.HideAsync();
+		}
     }
 }
--- APP/Components/Pages/Transferencia/Transferencia.razor.cs
+++ APP/Components/Pages/Transferencia/Transferencia.razor.cs
@@ -2,12 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using APP.Data.Modelos;
+using APP.Data.Servicios;
+using BlazorBootstrap;
 using Microsoft.AspNetCore.Components;
 
 namespace APP.Components.Pages.Transferencia
 {
     public partial class Transferencia : ComponentBase
     {
-        
+        [Inject]
+		public NavigationManager Navigation { get; set; }
+
+		[Inject]
+		public MovimientoServicio movimientoServicio { get; set; }
+
+		public Movimiento movimiento = new Movimiento();
+
+		private Modal modal = default!;
+
+		protected override async Task OnInitializedAsync()
+        {
+            movimiento.Fecha = DateTime.Now;
+        }
+        public async void Transferir()
+        {
+			
+		}
+
+        public void Borrar() {
+            
+        }
+
+		public async Task CerrarModal()
+		{
+			await modal.HideAsync();
+		}
     }
 }
