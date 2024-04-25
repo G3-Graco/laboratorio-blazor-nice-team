611cc39f2a3ba3aba3a19511d68c08314b881029
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-04-25 17:34:06 -0400

    Modificación en proceso

---
+++ APP/Data/Servicios/DocumentoServicio.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace APP.Data.Servicios
+{
+    public class DocumentoServicio
+    {
+        public readonly ProtectedLocalStorage _protectedLocalStorage;
+        public DocumentoServicio(ProtectedLocalStorage protectedLocalStorage)
+        {
+            _protectedLocalStorage = protectedLocalStorage;
+        }
+    }
+}
