1562a22c5d52d4a87973f38baa1f3a316f66d7ce
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-04-29 16:47:10 -0400

    Un logro nunca antes visto en documentos gracias a user221387 💖

--- APP/Program.cs
+++ APP/Program.cs
@@ -1,5 +1,6 @@
 using APP.Components;
 using APP.Data.Servicios;
+using Tewr.Blazor.FileReader;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,8 @@ builder.Services.AddScoped<MovimientoServicio>();
 builder.Services.AddScoped<PagoServicio>();
 builder.Services.AddScoped<CuotaServicio>();
 builder.Services.AddScoped<DocumentoServicio>();
+
+builder.Services.AddFileReaderService();
 
 var app = builder.Build();
 
